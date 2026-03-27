ef4f535ae2b9a18919974a0048b4f87331b40d0d
vedant <vedgaur573@gmail.com>
2026-03-27 20:24:27 +0530

    final nltk fix

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import streamlit as st
 import pickle
 import nltk
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+from nltk.stem.porter import PorterStemmer
 import os
 
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_dir, exist_ok=True)
 
-nltk.download('punkt')
-nltk.download('stopwords')
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('punkt_tab', download_dir=nltk_data_dir)
+nltk.download('stopwords', download_dir=nltk_data_dir)
+
+nltk.data.path.append(nltk_data_dir)
 # setup
 ps = PorterStemmer()
 
